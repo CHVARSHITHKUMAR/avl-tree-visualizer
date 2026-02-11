40eb966709c78b6cfd4be40c2a7676c428334de9
CHVARSHITHKUMAR <2310030075@klh.edu.in>
2026-02-11 22:25:49 +0530

    Added Rotations

--- avl.js
+++ avl.js
@@ -16,3 +16,28 @@ function height(n) {
 function getBalance(n) {
     return n ? height(n.left) - height(n.right) : 0;
 }
+function rightRotate(y) {
+    let x = y.left;
+    let T2 = x.right;
+
+    x.right = y;
+    y.left = T2;
+
+    y.height = 1 + Math.max(height(y.left), height(y.right));
+    x.height = 1 + Math.max(height(x.left), height(x.right));
+
+    return x;
+}
+
+function leftRotate(x) {
+    let y = x.right;
+    let T2 = y.left;
+
+    y.left = x;
+    x.right = T2;
+
+    x.height = 1 + Math.max(height(x.left), height(x.right));
+    y.height = 1 + Math.max(height(y.left), height(y.right));
+
+    return y;
+}
